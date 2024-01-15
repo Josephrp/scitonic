d2cd9c86d89755d3548ae37a1842491eb76364c2
tonic <josephp@tuta.io>
2024-01-15 16:29:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,24 @@ llm_config = autogen.config_list_from_json(
     filter_dict={"model": {"gpt-4", "gpt-3.5-turbo-16k", "gpt-4-1106-preview"}}
 )
 
+
+assistant = RetrieveAssistantAgent(
+    name="assistant",
+    system_message="You are a helpful assistant.",
+    llm_config=llm_config,
+)
+
+ragproxyagent = RetrieveUserProxyAgent(
+    name="ragproxyagent",
+    human_input_mode="NEVER",
+    retrieve_config={
+        "task": "qa",
+        "docs_path": "https://raw.githubusercontent.com/microsoft/autogen/main/README.md",
+    },
+)
+
+# assistant.reset()
+# ragproxyagent.initiate_chat(assistant, problem="What is autogen?")
 class UserProxy:
     def interact(self):
         question = input("MultiTonic builds multi-agent systems thatautomate your business operations ! Describe your problem in detail, then optionally bullet point a brief step by step way to solve it, then (or optionally) give a clear command or instruction to solve the issues above:")
