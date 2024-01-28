58531db3ce37b2b1f9c821a447eec57bcd4393c0
Tonic <josephrichardpollack@gmail.com>
2024-01-28 20:41:21 +0100

    add example output

--- src/mapper/scimap.py
+++ src/mapper/scimap.py
@@ -10,7 +10,7 @@ class scimap:
         messages = [
             {
                 "role": "system",
-                "content": "You are a subject matter technical expert. You select ONLY ONE from the list provided. ALWAYS respond in complete JSON. Always respond with the best possible team selected with YES or NO. ONLY\nselect ONE TEAM:\n        \"Team\": {\n          \"ClimateTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if it the task might relate to climate and environmental science\"\n          },\n          \"Covid19Team\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the user requires engaging with covid19topics\"\n          },\n          \"FinanceTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team the user requires analysis and advice about financials and financial literature\"\n          },\n          \"CodingTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the taskk requires producing code or technology\"\n          },\n          \"DebateTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the user requires debating a topic thoroughly\"\n          },\n          \"HomeworkTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the user requires help with homework or coursework\"\n          },\n          \"ConsultingTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the user requires business consulting\"\n          }\n        }  }\n    }\n  }\n]   \"required\": [\"ClimateTeam\", \"Covid19Team\" , \"FinanceTeam\", \"CodingTeam\" , \"DebateTeam\" , \"HomeworkTeam\" , \"ConsultingTeam\"]\n  "
+                "content": "You are a subject matter technical expert. You select ONLY ONE from the list provided. ALWAYS respond in complete JSON. Always respond with the best possible team selected with YES or NO. ONLY\nselect ONE TEAM:\n        \"Team\": {\n          \"ClimateTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if it the task might relate to climate and environmental science\"\n          },\n          \"Covid19Team\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the user requires engaging with covid19topics\"\n          },\n          \"FinanceTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team the user requires analysis and advice about financials and financial literature\"\n          },\n          \"CodingTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the taskk requires producing code or technology\"\n          },\n          \"DebateTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the user requires debating a topic thoroughly\"\n          },\n          \"HomeworkTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the user requires help with homework or coursework\"\n          },\n          \"ConsultingTeam\": {\n            \"type\": \"boolean\",\n            \"description\": \"select this team if the user requires business consulting\"\n          }\n        }  }\n    }\n  }\n]   \"required\": [\"ClimateTeam\", \"Covid19Team\" , \"FinanceTeam\", \"CodingTeam\" , \"DebateTeam\" , \"HomeworkTeam\" , \"ConsultingTeam\"]\n ```json\n{\n  \"Team\": {\n    \"ClimateTeam\": \"NO\",\n    \"Covid19Team\": \"NO\",\n    \"FinanceTeam\": \"NO\",\n    \"CodingTeam\": \"YES\",\n    \"DebateTeam\": \"NO\",\n    \"HomeworkTeam\": \"NO\",\n    \"ConsultingTeam\": \"NO\"\n  }\n}\n``` "
             },
             {
                 "role": "user",
@@ -28,3 +28,29 @@ class scimap:
             presence_penalty=presence_penalty
         )
         return response
+
+# Example Output
+
+# {
+#     "id": "chatcmpl-8m5ISPuFuvc6coyEZAYOV9VrlcvBU",
+#     "object": "chat.completion",
+#     "created": 1706470796,
+#     "model": "gpt-4-0125-preview",
+#     "choices": [
+#         {
+#             "index": 0,
+#             "message": {
+#                 "role": "assistant",
+#                 "content": "```json\n{\n  \"Team\": {\n    \"ClimateTeam\": \"NO\",\n    \"Covid19Team\": \"NO\",\n    \"FinanceTeam\": \"NO\",\n    \"CodingTeam\": \"YES\",\n    \"DebateTeam\": \"NO\",\n    \"HomeworkTeam\": \"NO\",\n    \"ConsultingTeam\": \"NO\"\n  }\n}\n```"
+#             },
+#             "logprobs": null,
+#             "finish_reason": "stop"
+#         }
+#     ],
+#     "usage": {
+#         "prompt_tokens": 362,
+#         "completion_tokens": 74,
+#         "total_tokens": 436
+#     },
+#     "system_fingerprint": "fp_376b7f78b9"
+# }
